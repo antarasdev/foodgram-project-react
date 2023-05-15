6f31df749459f5b94566bb03f8effb1b65926c6c
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-15 11:39:07 +0300

    fix user

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 from django.db.models import UniqueConstraint
 
-from .models import User
+User = get_user_model()
 
 
 class Ingredient(models.Model):
