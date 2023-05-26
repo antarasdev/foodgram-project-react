1cd7bca4849398fe03460d00a9b5008f4db3d3fd
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-26 11:32:25 +0300

    management add

---
+++ backend/recipes/management/commands/load_data.py
@@ -0,0 +1,19 @@
+import json
+
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext_lazy as _
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = _('Загрузить ингредиенты')
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.WARNING(_('Загрузка ингредиентов')))
+        with open('backend/ingredients.json', encoding='utf-8') as file:
+            ingredients = json.loads(file.read())
+            for ingredient in ingredients:
+                Ingredient.objects.get_or_create(**ingredient)
+
+        self.stdout.write(self.style.SUCCESS(_('Ингредиенты загружены')))
