36893472125b62c79cd779e0ee44990c01814e64
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-30 23:06:24 +0300

    fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,7 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 3
     page_size_query_param = 'limit'
-    max_page_size = 20
-    page_query_param = 'page'
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -104,7 +104,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'],
 
-    'DEFAULT_PAGINATION_CLASS': ['api.pagination.CustomPagination'],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
