1c02f14e12d16bf323d99725942a316a9494741f
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-30 22:18:12 +0300

    fix pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.paginations.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
 }
 
 DJOSER = {
