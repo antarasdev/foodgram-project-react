92c28ecb3a27f6242782425eb30c4648ebbe7ab3
Андрей Тарасов <baby-shit@yandex.ru>
2023-05-31 09:23:37 +0300

    fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,7 +1,7 @@
-from django.conf import settings
+from foodgram.settings import PAGE_SIZE
 from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = settings.PAGE_SIZE
+    page_size = PAGE_SIZE
     page_size_query_param = 'limit'
